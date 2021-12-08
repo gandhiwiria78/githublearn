9b7e9d11af085cd373ba038333f685427f28d823
Gandhiwiria <gundhiwr56@gmail.com>
2021-12-08 10:33:47 +0700

    first commit

---
+++ android/app/src/main/java/com/cabrider/grab_rider/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cabrider.grab_rider;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/mainpage.dart
@@ -0,0 +1,19 @@
+import'package:flutter/material.dart';
+
+class Mainpage extends StatefulWidget {
+  const Mainpage({Key? key}) : super(key: key);
+
+  @override
+  _MainpageState createState() => _MainpageState();
+}
+
+class _MainpageState extends State<Mainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Page'),
+      ),
+    );
+  }
+}
